67489db11ccac862d56edb190989c48846f952dd
bwerapol <bwerapol@gmail.com>
2025-06-12 18:42:58 +0200

    App building

---
+++ run_dict_app.sh
@@ -0,0 +1,2 @@
+cd /Users/user/Documents/DictApp/
+/usr/local/bin/python3 thai_slang_dict_main.py
---
+++ run_gui_wrapper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Users/user/Documents/DictApp || exit
+/usr/local/bin/python3 thai_slang_dict_main.py
